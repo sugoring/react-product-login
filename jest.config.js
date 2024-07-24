391da8a769b05a576d8c639c10537ae14734c656
yuni <hys990730@gmail.com>
2024-07-24 15:29:40 +0900

    refactor: lint룰 적용

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-    },
-  };
-  
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
